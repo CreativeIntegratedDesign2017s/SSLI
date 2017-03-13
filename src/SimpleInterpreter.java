6e2e6604d2a98e330fbc736c3af75eca09b6fc78
holim0711 <holim0711@outlook.com>
2017-03-13 17:21:49 +0900

    Modify Structure & Simple main()
    rename dependencies -> lib (Also project setting, too)
    rename Test->test
    
    Changes to be committed:
    	deleted:    idea_base/
    	renamed:    dependencies/antlr-4.6-complete.jar -> lib/antlr-4.6-complete.jar

--- src/SimpleInterpreter.java
+++ src/SimpleInterpreter.java
@@ -1,8 +1,26 @@
 /**
  * Created by HeeM on 2017-03-13.
+ * Simply implemented for System.in by Holim on 2017-03-13.
  */
+
+import java.io.*;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
 public class SimpleInterpreter {
-    static public void main(String args[]){
-    	System.out.println("Hello, World!");
+    static public void main(String args[]) throws IOException {
+    	System.out.println("Welcome to Simple Interpreter!");
+
+        ANTLRInputStream input = new ANTLRInputStream(System.in);
+
+        SimpleLexer lexer = new SimpleLexer(input);
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        SimpleParser parser = new SimpleParser(tokens);
+
+        ParseTree tree = parser.prgm();
+
+        System.out.println(tree.toStringTree(parser));
     }
 }
