ad74209aa5025bd79dfca59f3c6ceb0f23c2e8f0
BettyKwon <kbk2289@gmail.com>
2017-05-12 13:21:00 +0900

    structure

--- src/ASTBuilder.java
+++ src/ASTBuilder.java
@@ -27,14 +27,43 @@ class Stmt_listNode extends ASTNode {
 }
 
 class ExprNode extends ASTNode {}
-class BinaryInfixExpr extends ExprNode {
-    public ExprNode Oprnd1;
-    public Token op;
-    public ExprNode Oprnd2;
+class BinaryExpr extends ExprNode {
+    public ExprNode oprnd1;
+    public ExprNode oprnd2;
 }
-class AddSubExpr extends BinaryInfixExpr {}
+class PowExpr extends BinaryExpr {}
+class MulExpr extends BinaryExpr {}
+class DivExpr extends BinaryExpr {}
+class AddExpr extends BinaryExpr {}
+class SubExpr extends BinaryExpr {}
+class AndExpr extends BinaryExpr {}
+class OrExpr extends BinaryExpr {}
 
+class LTExpr extends BinaryExpr {}
+class LEExpr extends BinaryExpr {}
+class EQExpr extends BinaryExpr {}
+class NEQExpr extends BinaryExpr {}
+class GEExpr extends BinaryExpr {}
+class GTExpr extends BinaryExpr {}
+
+class UnaryExpr extends ExprNode {
+    public ExprNode oprnd;
+}
+class PlusExpr extends UnaryExpr {}
+class MinusExpr extends UnaryExpr {}
+class NegativeExpr extends UnaryExpr {}
+
+class ProcCallExpr extends ExprNode {
+    List<ExprNode> argumentList;
+}
+class SubscriptExpr extends ExprNode {}
 class SubstrExpr extends ExprNode {}
+
+class TerminalExpr extends ExprNode {}
+class ID extends TerminalExpr {}
+class INT extends TerminalExpr {}
+class BOOL extends TerminalExpr {}
+class STR extends TerminalExpr {}
 
 class ASTBuilder {}
 
