311f9ccb1fe3310a439d72ecd3d9fdb2d23905da
holim0711 <holim0711@outlook.com>
2017-05-25 23:07:18 +0900

    Load IR

--- src/SimpleVM/Instruction.java
+++ src/SimpleVM/Instruction.java
@@ -1,54 +1,102 @@
 package SimpleVM;
 
-class Instruction {
-    enum Operator {
-        NOP,
-        MOVE,
-        LOAD,
-        LEA,
-        UMN,
-        NOT,
-        ADD,
-        SUB,
-        MUL,
-        DIV,
-        POW,
-        EQ,
-        LT,
-        LE,
-        GT,
-        GE,
-        NE,
-        JMP,
-        TEST,
-        CALL,
-        RET,
-        GET_TABLE,
-        NEW_TABLE
-    }
-    Operator op;
-}
-
 class Operand {
     enum AddrMode {
         REGISTER,
         CONSTANT
     }
+
     AddrMode mode;
     Object val;
+
+    Operand(String str) {
+        if (str.charAt(0) == '$') {
+            this.mode = AddrMode.CONSTANT;
+            switch(str.charAt(1)) {
+                case '"':
+                    this.val = str.substring(1, str.length() - 1);
+                    break;
+                case '0':
+                    /* Special Case Optimization */
+                    this.val = 0;
+                    break;
+                case '1': case '2': case '3':
+                case '4': case '5': case '6':
+                case '7': case '8': case '9':
+                    this.val = Integer.valueOf(str.substring(0, str.length() - 1));
+                    break;
+                case 't':
+                    this.val = true;
+                    break;
+                case 'f':
+                    this.val = false;
+                    break;
+            }
+        }
+        else {
+            this.mode = AddrMode.REGISTER;
+            this.val = Integer.valueOf(str.substring(0, str.length() - 1));
+        }
+    }
 }
 
-class InstAddr1 extends Instruction {
-    Operand A;
+class Instruction {
+    enum Operator {
+        NOP,
+        MOVE, LOAD, LEA,
+        UMN, NOT,
+        ADD, SUB, MUL, DIV, POW,
+        EQ, NE, LT, LE, GT, GE,
+        JMP, RET,
+        TEST, CALL,
+        GET_TABLE,
+        NEW_TABLE
+    }
+
+    Operator op;
+
+    static Instruction valueOf(String str) {
+        Instruction inst = null;
+        String[] token = str.split(" ");
+        Operator op = Operator.valueOf(token[0]);
+        switch (op) {
+            case NOP:
+                InstAddr0 addr0 = new InstAddr0();
+                addr0.op = op;
+                inst = addr0;
+                break;
+            case JMP:
+            case RET:
+                InstAddr1 addr1 = new InstAddr1();
+                addr1.op = op;
+                addr1.A = (token.length == 1) ? null : new Operand(token[1]);
+                inst =  addr1;
+                break;
+            case MOVE: case LOAD: case LEA:
+            case UMN: case NOT:
+            case TEST: case CALL:
+                InstAddr2 addr2 = new InstAddr2();
+                addr2.op = op;
+                addr2.A = new Operand(token[1]);
+                addr2.B = new Operand(token[2]);
+                inst = addr2;
+                break;
+            case ADD: case SUB: case MUL: case DIV: case POW:
+            case LE: case LT: case EQ: case NE: case GT: case GE:
+            case GET_TABLE: case NEW_TABLE:
+                InstAddr3 addr3 = new InstAddr3();
+                addr3.op = op;
+                addr3.A = new Operand(token[1]);
+                addr3.B = new Operand(token[2]);
+                addr3.C = new Operand(token[3]);
+                inst = addr3;
+                break;
+        }
+        return inst;
+    }
 }
 
-class InstAddr2 extends Instruction {
-    Operand A;
-    Operand B;
-}
-
-class InstAddr3 extends Instruction {
-    Operand A;
-    Operand B;
-    Operand C;
-}
+class InstAddr0 extends Instruction { }
+class InstAddr1 extends Instruction { Operand A; }
+class InstAddr2 extends Instruction { Operand A, B; }
+class InstAddr3 extends Instruction { Operand A, B, C; }
--- src/SimpleVM/SimpleVM.java
+++ src/SimpleVM/SimpleVM.java
@@ -1,35 +1,56 @@
 package SimpleVM;
-
 import java.util.*;
 
-class Procedure {
-    String name;
-    ArrayList<Instruction> inst;
-}
+class InstRegister {
+    ArrayList<Instruction> proc;
+    Integer idx;
 
-public class SimpleVM
-{
-    private Object[] register;
-    private Integer PrgmCnt;
-    private Integer InstReg;
-    private Integer FlagReg;
-
-    Stack<Procedure> callStack;
-    HashMap<String, Procedure> procMap;
-
-
-    SimpleVM() {
-        register = new Object[256];
-        PrgmCnt = 0;
-        InstReg = 0;
-        FlagReg = 0;
-
-        callStack = new Stack<>();
-        procMap = new HashMap<>();
+    InstRegister() {
+        proc = new ArrayList<>();
+        idx = 0;
     }
 
-    void LoadInst(String inst) {}
-    void loadPrgm(String filename) {}
+    InstRegister(ArrayList<Instruction> inst) {
+        proc = inst;
+        idx = 0;
+    }
+}
+
+enum FlagRegister {
+    Default,
+    Invalid_IR,
+    Invalid_Reg,
+    Overflow,
+    Underflow,
+    Div_Zero,
+    Out_Range,
+    Out_Proc
+}
+
+public class SimpleVM  {
+    private Object[] Register;
+    private InstRegister InstReg;
+    private FlagRegister FlagReg;
+    private Stack<InstRegister> CallStk;
+    private HashMap<String, ArrayList<Instruction>> ProcMap;
+
+    SimpleVM() {
+        Register = new Object[256];
+        InstReg = new InstRegister();
+        FlagReg = FlagRegister.Default;
+        CallStk = new Stack<>();
+        ProcMap = new HashMap<>();
+    }
+
+    SimpleVM(String filename) { /* Future Work */ }
+
+    void LoadIR(String proc, List<String> ir) {
+        LinkedList<Instruction> inst = new LinkedList<>();
+        for (String x : ir)
+            inst.add(Instruction.valueOf(x));
+        ProcMap.get(proc).addAll(inst);
+    }
+
     void step() {} // line by line
     void run() {}  // run the whole file
 }
