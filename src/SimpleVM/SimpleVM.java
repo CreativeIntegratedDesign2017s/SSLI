c19423c73727e941b6b3fbdc23bbe8f92f8776f7
holim0711 <holim0711@outlook.com>
2017-05-25 16:51:44 +0900

    hello hello

--- src/SimpleVM/SimpleVM.java
+++ src/SimpleVM/SimpleVM.java
@@ -2,18 +2,34 @@ package SimpleVM;
 
 import java.util.*;
 
+class Procedure {
+    String name;
+    ArrayList<Instruction> inst;
+}
+
 public class SimpleVM
 {
-    private Object[] DataMem;
-    private Instruction[] InstMem;
-
+    private Object[] register;
+    private Integer PrgmCnt;
     private Integer InstReg;
     private Integer FlagReg;
 
+    Stack<Procedure> callStack;
+    HashMap<String, Procedure> procMap;
+
+
     SimpleVM() {
-        DataMem = new Object[256];
-        InstMem = null;
+        register = new Object[256];
+        PrgmCnt = 0;
         InstReg = 0;
         FlagReg = 0;
+
+        callStack = new Stack<>();
+        procMap = new HashMap<>();
     }
+
+    void LoadInst(String inst) {}
+    void loadPrgm(String filename) {}
+    void step() {} // line by line
+    void run() {}  // run the whole file
 }
