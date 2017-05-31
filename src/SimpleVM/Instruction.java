2e88f623ce03f0cdcae5da2de1b072078a9a2a5f
holim0711 <holim0711@outlook.com>
2017-05-31 18:25:42 +0900

    VM new structure

--- src/SimpleVM/Instruction.java
+++ src/SimpleVM/Instruction.java
@@ -1,150 +1,57 @@
 package SimpleVM;
 
-class Operand {
-    enum AddrMode {
-        REGISTER,
-        CONSTANT
-    }
+enum Operator {
+    NOP,	MOVE,	LOAD,	UMN,	NOT,
+    ADD,	SUB,	MUL,	DIV,	POW,
+    EQ,		NE,		LT,		LE,		GT,		GE,
+    JMP,	RET,	TEST,	CALL,
+    GET_TABLE,		NEW_TABLE
+}
 
+class Operand {
+    enum AddrMode { GLOBAL, LOCAL, CONST }
     AddrMode mode;
     Object val;
 
-    Operand(String str) {
-        if (str.charAt(0) == '$') {
-            this.mode = AddrMode.CONSTANT;
-            switch(str.charAt(1)) {
-                case '"':
-                    this.val = str.substring(1, str.length() - 1);
-                    break;
-                case '0':
-                    /* Special Case Optimization */
-                    this.val = 0;
-                    break;
-                case '1': case '2': case '3':
-                case '4': case '5': case '6':
-                case '7': case '8': case '9':
-                    this.val = Integer.valueOf(str.substring(0, str.length() - 1));
-                    break;
-                case 't':
-                    this.val = true;
-                    break;
-                case 'f':
-                    this.val = false;
-                    break;
-            }
-        }
-        else {
-            this.mode = AddrMode.REGISTER;
-            this.val = Integer.valueOf(str.substring(0, str.length() - 1));
-        }
+    static private Object valueOf(String str) {
+        char c = str.charAt(0);
+        if ('0' <= c && c <= '9')
+            return Integer.valueOf(str);
+        else if (c == '"' && str.charAt(str.length() - 1) == '"')
+            return str.substring(1, str.length() - 1);
+        else
+            return (c == 't');
     }
 
-    /*
-     Get value of an operand regardless of its mode(Register or Constant
-     */
-    Object getValue(Object[] Register) {
-        if(this.mode == AddrMode.REGISTER) {
-            return Register[(int) this.val];
+    Operand(String str) {
+        switch (str.charAt(0)) {
+            case '$':
+                mode = AddrMode.CONST;
+                val = valueOf(str.substring(1));
+                break;
+            case 'G':
+                mode = AddrMode.GLOBAL;
+                val = Integer.valueOf(str.substring(1));
+                break;
+            default:
+                mode = AddrMode.LOCAL;
+                val = Integer.valueOf(str);
+                break;
         }
-        return this.val;
     }
 }
 
 class Instruction {
-    enum Operator {
-        NOP,
-        MOVE, LOAD, LEA,
-        UMN, NOT,
-        ADD, SUB, MUL, DIV, POW,
-        EQ, NE, LT, LE, GT, GE,
-        JMP, RET,
-        TEST, CALL,
-        GET_TABLE,
-        NEW_TABLE
-    }
-
     Operator op;
+    Operand[] oprnd;
 
     static Instruction valueOf(String str) {
-        Instruction inst = null;
         String[] token = str.split(" ");
-        Operator op = Operator.valueOf(token[0]);
-        switch (op) {
-            case NOP:
-                InstAddr0 addr0 = new InstAddr0();
-                addr0.op = op;
-                inst = addr0;
-                break;
-            case JMP:
-            case RET:
-                InstAddr1 addr1 = new InstAddr1();
-                addr1.op = op;
-                addr1.A = (token.length == 1) ? null : new Operand(token[1]);
-                inst =  addr1;
-                break;
-            case MOVE: case LOAD: case LEA:
-            case UMN: case NOT:
-            case TEST: case CALL:
-                InstAddr2 addr2 = new InstAddr2();
-                addr2.op = op;
-                addr2.A = new Operand(token[1]);
-                addr2.B = new Operand(token[2]);
-                inst = addr2;
-                break;
-            case ADD: case SUB: case MUL: case DIV: case POW:
-            case LE: case LT: case EQ: case NE: case GT: case GE:
-            case GET_TABLE: case NEW_TABLE:
-                InstAddr3 addr3 = new InstAddr3();
-                addr3.op = op;
-                addr3.A = new Operand(token[1]);
-                addr3.B = new Operand(token[2]);
-                addr3.C = new Operand(token[3]);
-                inst = addr3;
-                break;
-        }
+        Instruction inst = new Instruction();
+        inst.op = Operator.valueOf(token[0]);
+        inst.oprnd = new Operand[token.length - 1];
+        for (int i = 0; i < inst.oprnd.length; i++)
+            inst.oprnd[i] = new Operand(token[i + 1]);
         return inst;
     }
-
-    void run(Object[] Register) {}
-}
-
-class InstAddr0 extends Instruction { }
-class InstAddr1 extends Instruction { Operand A; }
-class InstAddr2 extends Instruction { Operand A, B; }
-class InstAddr3 extends Instruction {
-    Operand A, B, C;
-
-    @Override
-    void run(Object[] Register) {
-        try {
-            if (A.mode == Operand.AddrMode.REGISTER) {
-                switch (this.op) {
-                    case ADD:
-                        Register[(int) A.val] = Math.addExact((int) B.getValue(Register), (int) C.getValue(Register));
-                        break;
-                    case SUB:
-                        Register[(int) A.val] = Math.subtractExact((int) B.getValue(Register), (int) C.getValue(Register));
-                        break;
-                    case MUL:
-                        Register[(int) A.val] = Math.multiplyExact((int) B.getValue(Register), (int) C.getValue(Register));
-                        break;
-                    case DIV:
-                        Register[(int) A.val] = Math.floorDiv((int) B.getValue(Register), (int) C.getValue(Register));
-                        break;
-                    case POW:
-                        Register[(int) A.val] = Math.pow((int) B.getValue(Register), (int) C.getValue(Register));
-                        break;
-                }
-            }
-            else {
-                throw new Exception("Wrong Destination Type");
-            }
-        }
-        catch(Exception exc) {
-            if(exc instanceof ArithmeticException) { /* TODO */ }   // catches overflow, underflow and div by 0s
-            else if(exc instanceof ArrayIndexOutOfBoundsException) { /* TODO */ }
-            else { /* TODO */ }
-        }
-    }
-
 }
--- src/SimpleVM/SimpleVM.java
+++ src/SimpleVM/SimpleVM.java
@@ -1,56 +1,254 @@
 package SimpleVM;
 import java.util.*;
 
-class InstRegister {
-    ArrayList<Instruction> proc;
-    Integer idx;
+class DataRegister {
+    Object[] data;
+    int base;
 
-    InstRegister() {
-        proc = new ArrayList<>();
-        idx = 0;
+    DataRegister() {
+        data = new Object[256];
     }
 
-    InstRegister(ArrayList<Instruction> inst) {
+    Object read(Operand idx) {
+        switch (idx.mode) {
+            case CONST:
+                return idx.val;
+            case LOCAL:
+                return data[base + (int)idx.val];
+            case GLOBAL:
+                return data[(int)idx.val];
+            default:
+                return null;
+        }
+    }
+
+    void write(Operand idx, Object val) {
+        switch (idx.mode) {
+            case LOCAL:
+                data[base + (int)idx.val] = val;
+                break;
+            case GLOBAL:
+                data[(int)idx.val] = val;
+                break;
+            default:
+                break;
+        }
+    }
+
+    private Object[] recursiveTable(List<Integer> size) {
+        return null;
+    }
+
+    void makeTable(Operand a, Operand b, Operand c) {
+        return;
+    }
+
+    void readTable(Operand a, Operand b, Operand c) {
+        data[base + (int)a.val] = ((Object[])data[base + (int)b.val])[(int)data[base + (int)c.val]];
+    }
+}
+
+class InstRegister {
+    Instruction[] proc;
+    int idx;
+
+    InstRegister() {
+        proc = new Instruction[0];
+    }
+
+    InstRegister(Instruction[] inst) {
         proc = inst;
-        idx = 0;
+    }
+
+    Instruction getInst() {
+        return proc[idx++];
     }
 }
 
 enum FlagRegister {
-    Default,
-    Invalid_IR,
-    Invalid_Reg,
-    Overflow,
-    Underflow,
-    Div_Zero,
-    Out_Range,
-    Out_Proc
+    Default,	Overflow,	Underflow,
+    Div_Zero,	Out_Arr,	Out_Proc,
+    Unhandled
+}
+
+class CallStack {
+    static class ProcContext {
+        Instruction[] proc;
+        int idx;
+        int base;
+    }
+
+    private Stack<ProcContext> stack;
+
+    CallStack() {
+        stack = new Stack<>();
+    }
+
+    void push(Instruction[] proc, int idx, int base) {
+        ProcContext ctx = new ProcContext();
+        ctx.proc = proc;
+        ctx.idx = idx;
+        ctx.base = base;
+        stack.push(ctx);
+    }
+
+    ProcContext pop() {
+        return stack.pop();
+    }
 }
 
 public class SimpleVM  {
-    private Object[] Register;
-    private InstRegister InstReg;
-    private FlagRegister FlagReg;
-    private Stack<InstRegister> CallStk;
-    private HashMap<String, ArrayList<Instruction>> ProcMap;
+    private DataRegister dataReg;
+    private InstRegister instReg;
+    private FlagRegister flagReg;
+    private CallStack callStk;
+    private HashMap<String, Instruction[]> procMap;
 
     SimpleVM() {
-        Register = new Object[256];
-        InstReg = new InstRegister();
-        FlagReg = FlagRegister.Default;
-        CallStk = new Stack<>();
-        ProcMap = new HashMap<>();
+        dataReg = new DataRegister();
+        instReg = new InstRegister();
+        flagReg = FlagRegister.Default;
+        callStk = new CallStack();
+        procMap = new HashMap<>();
     }
 
     SimpleVM(String filename) { /* Future Work */ }
 
-    void LoadIR(String proc, List<String> ir) {
-        LinkedList<Instruction> inst = new LinkedList<>();
-        for (String x : ir)
-            inst.add(Instruction.valueOf(x));
-        ProcMap.get(proc).addAll(inst);
+    void loadIR(String pid, List<String> ir) {
+        Instruction[] inst = ir.stream()
+                .map(Instruction::valueOf)
+                .toArray(size -> new Instruction[size]);
+        procMap.put(pid, inst);
     }
 
-    void step() {} // line by line
-    void run() {}  // run the whole file
+    boolean step() {
+        Instruction inst = instReg.getInst();
+        try {
+            switch (inst.op) {
+                case NOP:
+                    break;
+                case MOVE:
+                    // TODO: Deep Copy
+                    break;
+                case LOAD:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            dataReg.read(inst.oprnd[1])
+                    );
+                    break;
+                case UMN:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            -(Integer) dataReg.read(inst.oprnd[1])
+                    );
+                    break;
+                case NOT:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            !(Boolean) dataReg.read(inst.oprnd[1])
+                    );
+                    break;
+                case ADD:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            ((Integer) dataReg.read(inst.oprnd[1]) + (Integer) dataReg.read(inst.oprnd[2]))
+                    );
+                    break;
+                case SUB:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            ((Integer) dataReg.read(inst.oprnd[1]) - (Integer) dataReg.read(inst.oprnd[2]))
+                    );
+                    break;
+                case MUL:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            ((Integer) dataReg.read(inst.oprnd[1]) * (Integer) dataReg.read(inst.oprnd[2]))
+                    );
+                    break;
+                case DIV:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            ((Integer) dataReg.read(inst.oprnd[1]) / (Integer) dataReg.read(inst.oprnd[2]))
+                    );
+                    break;
+                case POW:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            Math.pow((Integer) dataReg.read(inst.oprnd[1]), (Integer) dataReg.read(inst.oprnd[2]))
+                    );
+                    break;
+                case EQ:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            Objects.equals(dataReg.read(inst.oprnd[1]), dataReg.read(inst.oprnd[2]))
+                    );
+                    break;
+                case NE:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            !Objects.equals(dataReg.read(inst.oprnd[1]), dataReg.read(inst.oprnd[2]))
+                    );
+                    break;
+                case LT:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            ((Integer) dataReg.read(inst.oprnd[1]) < (Integer) dataReg.read(inst.oprnd[2]))
+                    );
+                    break;
+                case LE:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            ((Integer) dataReg.read(inst.oprnd[1]) <= (Integer) dataReg.read(inst.oprnd[2]))
+                    );
+                    break;
+                case GT:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            ((Integer) dataReg.read(inst.oprnd[1]) > (Integer) dataReg.read(inst.oprnd[2]))
+                    );
+                    break;
+                case GE:
+                    dataReg.write(
+                            inst.oprnd[0],
+                            ((Integer) dataReg.read(inst.oprnd[1]) >= (Integer) dataReg.read(inst.oprnd[2]))
+                    );
+                    break;
+                case JMP:
+                    instReg.idx += (Integer) dataReg.read(inst.oprnd[0]);
+                    break;
+                case TEST:
+                    if ((Boolean) dataReg.read(inst.oprnd[0]))
+                        instReg.idx += (Integer) dataReg.read(inst.oprnd[1]);
+                    break;
+                case CALL:
+                    callStk.push(instReg.proc, instReg.idx, dataReg.base);
+                    instReg = new InstRegister(procMap.get((String) dataReg.read(inst.oprnd[0])));
+                    dataReg.base = (Integer) inst.oprnd[1].val;
+                    break;
+                case RET:
+                    if (inst.oprnd.length != 0)
+                        dataReg.data[dataReg.base] = dataReg.read(inst.oprnd[0]);
+                    CallStack.ProcContext ctx = callStk.pop();
+                    instReg.proc = ctx.proc;
+                    instReg.idx = ctx.idx;
+                    dataReg.base = ctx.base;
+                    break;
+                case GET_TABLE:
+                    dataReg.readTable(inst.oprnd[0], inst.oprnd[1], inst.oprnd[2]);
+                    break;
+                case NEW_TABLE:
+                    dataReg.makeTable(inst.oprnd[0], inst.oprnd[1], inst.oprnd[2]);
+                    break;
+            }
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
+
+    boolean run() {
+        return step();
+    }
 }
